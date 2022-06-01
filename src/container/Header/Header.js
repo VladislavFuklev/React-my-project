2e789d79d292046e1298583b9751bb1b57d9117f
Vladislav <vladfyklev@gmail.com>
2022-06-01 22:41:09 +0300

    добавил папку img

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,11 +1,53 @@
 import React from "react";
 import { Container } from "@mui/material";
-import "Header.scss";
+import "./Header.css";
 
 export function Headers() {
   return (
     <>
-      <div className="header"></div>
+      <div className="header">
+        <Container>
+          <div className="header__container">
+            <div className="header__logo">
+              <img src="" alt=""></img>
+            </div>
+            <div className="header__menu">
+              <ul className="menu__list">
+                <li className="list__item">
+                  <a className="list__link home__link" href="">
+                    Home
+                  </a>
+                </li>
+                <li className="list__item">
+                  <a className="list__link" href="">
+                    Destinations
+                  </a>
+                </li>
+                <li className="list__item">
+                  <a className="list__link" href="">
+                    Who we are
+                  </a>
+                </li>
+                <li className="list__item">
+                  <a className="list__link" href="">
+                    Blog
+                  </a>
+                </li>
+                <li className="list__item">
+                  <a className="list__link" href="">
+                    Contact
+                  </a>
+                </li>
+                <li className="list__item">
+                  <a className="list__link" href="">
+                    Buy now
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
-  )
+  );
 }
