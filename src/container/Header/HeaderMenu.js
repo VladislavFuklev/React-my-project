a184c0c3830c52fae21544b42ca91f01ad9bfbfa
Vladislav <vladfyklev@gmail.com>
2022-06-07 10:26:34 +0300

    ...

--- src/container/Header/HeaderMenu.js
+++ src/container/Header/HeaderMenu.js
@@ -5,32 +5,32 @@ export function HeaderMenu() {
     <div className="header__menu">
       <ul className="menu__list">
         <li className="list__item">
-          <a className="list__link home__link" href="#">
+          <a className="list__link home__link" href="#/">
             Home
           </a>
         </li>
         <li className="list__item">
-          <a className="list__link" href="#">
+          <a className="list__link" href="#/">
             Destinations
           </a>
         </li>
         <li className="list__item">
-          <a className="list__link" href="#">
+          <a className="list__link" href="#/">
             Who we are
           </a>
         </li>
         <li className="list__item">
-          <a className="list__link" href="#">
+          <a className="list__link" href="#/">
             Blog
           </a>
         </li>
         <li className="list__item">
-          <a className="list__link" href="#">
+          <a className="list__link" href="#/">
             Contact
           </a>
         </li>
         <li className="list__item">
-          <a className="list__link" href="#">
+          <a className="list__link" href="#/">
             Buy now
           </a>
         </li>
