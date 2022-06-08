70987104f176dfddc15bfc08dfee076744816393
Vladislav <vladfyklev@gmail.com>
2022-06-08 19:12:12 +0300

    добавил иконки

--- src/container/Footer/FooterIconts.js
+++ src/container/Footer/FooterIconts.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
 
 export function FooterIcons() {
   return (
     <>
-    <div className="footer-descr">
-    </div>
+      <div className="footer-descr">
+        <a href="#/"><FaInstagramSquare /></a>
+        <a href="#/"><FaTelegram /></a>
+        <a href="#/"> <FaTwitterSquare/></a>
+        <a href="#/"> <FaFacebookSquare/></a>
+      </div>
     </>
   );
 }
-
-
